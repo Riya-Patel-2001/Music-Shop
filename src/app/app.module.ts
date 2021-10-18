afbaf64e1521ce331f812027bd06af95d79beacd
Riya-Patel-2001 <69039370+Riya-Patel-2001@users.noreply.github.com>
2021-10-18 12:59:37 -0400

    Music shop project

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+
+//import { AppComponent } from './app.component';
+import { HomeListComponent } from './home-list/home-list.component';
+
+@NgModule({
+  declarations: [
+    HomeListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [HomeListComponent]
+})
+export class AppModule { }
